b86906a5752a3618b9fdf801b5093b96eb654169
Cristian <camilo.santamaria322@gmail.com>
2021-06-06 18:25:01 -0500

    Se agrega funcionalidad de reversar movimientos y de volver a iniciar se utilizo concat, se envio funcionalidad al componente Game como componente principal trasladando funciones

--- src/index.js
+++ src/index.js
@@ -15,53 +15,20 @@ function Square(props) {
 
 //Se envia el value al componente Square
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      //Se define un arreglo con 9 valores y con el metodo .fill de javascript le digo que los 9 seran null
-      squares: Array(9).fill(null),
-      //Se deine un nuevo parametro indicando si ya se marco la X para que nos aparezca la O
-      xIsNext: true,
-    };
-  }
-  //Se agrega el evento handleClick este me pintara las X o O
-  handleClick(i) {
-    //El metodo slice devuelve una copia del array inicial sin modificarlo.(recibe el parametro de inicio y el de fin)
-    const squares = this.state.squares.slice();
-    //Se agrega validacion para verificar si ya se hizo triqui
-    if(calculateWinner(squares) || squares[i]){
-        //Al tener este return se saldra y no ejecutara mas la funcion por lo que no pintara
-        return;
-    }
-    //Se utiliza un operador ternario para definir si va X o O para eso utilizaremos nuestra variable xIsNext
-    squares[i] = this.state.xIsNext ? "X" : "O";
-    //Como el set es el que me envia el dato aca estamos mandando el que tenemos definido en la linea anterior
-    this.setState({ squares: squares, xIsNext: !this.state.xIsNext });
-  }
   renderSquare(i) {
     return (
       <Square
-        value={this.state.squares[i]}
+        value={this.props.squares[i]}
         onClick={() => {
-          this.handleClick(i);
+          this.props.onClick(i);
         }}
       />
     );
   }
 
   render() {
-    //Ahora llamaremos el ganador 
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if(winner){
-        status = 'Winner:' +winner;
-    }else{
-        status = 'Next player:' + (this.state.xIsNext ? 'X' : 'O');
-    }
-
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -83,15 +50,84 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+  //Se agrega este constructor con el fin de manejar el historial
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [
+        {
+          squares: Array(9).fill(null),
+        }
+      ],
+      stepNumber: 0,
+      xIsNext: true
+    };
+  }
+  //Se agrega el evento handleClick este me pintara las X o O
+  handleClick(i) {
+    //Se agrega el history
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    //El metodo slice devuelve una copia del array inicial sin modificarlo.(recibe el parametro de inicio y el de fin)
+    const squares = current.squares.slice();
+    //Se agrega validacion para verificar si ya se hizo triqui
+    if (calculateWinner(squares) || squares[i]) {
+      //Al tener este return se saldra y no ejecutara mas la funcion por lo que no pintara
+      return;
+    }
+    //Se utiliza un operador ternario para definir si va X o O para eso utilizaremos nuestra variable xIsNext
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    this.setState({
+      //Manejando lo de la mutabilidad con concat no se muta el array anterior por esto es preferido me agrega el elemento pero no me cambia el original
+      history: history.concat([
+        {
+          squares: squares,
+        },
+      ]),
+      stepNumber: history.length,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: step % 2 === 0,
+    });
+  }
+
   render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc = move ? "Go to move #" + move : "Go to game start";
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+
+    let status;
+    if (winner) {
+      status = "Winner: " + winner;
+    } else {
+      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+    }
+
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
+          <div>{status}</div>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
